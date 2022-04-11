6f30356eba88436974ce98f9c285304e36420c2c
Julius Michaelis <gitter@liftm.de>
2022-04-11 12:51:07 +0900

    Clippy

--- src/imp.rs
+++ src/imp.rs
@@ -83,12 +83,8 @@ fn recurse_through_struct_fields(
             modify_punctuated(&mut n.fields, |field| {
                 let name_hint = field.name.to_string().to_case(Case::Pascal);
                 let name_hint = Ident::new(&name_hint, field.name.span());
-                let ttok = recurse_through_type(
-                    &field.ty.tokens[..],
-                    &strike_attrs,
-                    ret,
-                    &Some(name_hint),
-                );
+                let ttok =
+                    recurse_through_type(&field.ty.tokens[..], strike_attrs, ret, &Some(name_hint));
                 field.ty.tokens = ttok;
             });
             n.update_tokens();
@@ -96,8 +92,7 @@ fn recurse_through_struct_fields(
         StructFields::Unit => (),
         StructFields::Tuple(t) => {
             modify_punctuated(&mut t.fields, |field| {
-                let ttok =
-                    recurse_through_type(&field.ty.tokens[..], &strike_attrs, ret, name_hint);
+                let ttok = recurse_through_type(&field.ty.tokens[..], strike_attrs, ret, name_hint);
                 field.ty.tokens = ttok;
             });
             t.update_tokens();
@@ -148,7 +143,7 @@ fn recurse_through_type(
                     return vec![TokenTree::Punct(Punct::new('!', Spacing::Alone))];
                 }
             };
-            let head = head.into_iter().cloned().collect::<TokenStream>();
+            let head = head.iter().cloned().collect::<TokenStream>();
             let newthing = quote! {#head #kw #name #body};
             recurse_through_definition(newthing, strike_attrs.clone(), ret);
             vec![TokenTree::Ident(name)]
@@ -158,10 +153,7 @@ fn recurse_through_type(
             if open.as_char() == '<' && close.as_char() == '>' =>
         {
             let subs = inner
-                .split(|tt| match tt {
-                    TokenTree::Punct(comma) if comma.as_char() == ',' => true,
-                    _ => false,
-                })
+                .split(|tt| matches!(tt, TokenTree::Punct(comma) if comma.as_char() == ','))
                 .map(|sub| recurse_through_type(sub, strike_attrs, ret, name_hint));
             [
                 &[typ.clone(), TokenTree::Punct(open.clone())][..],
